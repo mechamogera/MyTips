8b846097aca127aac4f20e36d9f62d4d629081db
mechamogera <mechamogera@gmail.com>
2012-05-23 10:44:26 +0900

    added sample rails project

---
+++ sample-blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleBlog::Application.config.secret_token = '16d36b1bf3fdef806cf312a65196cc905a16ce4f9a57d46e22447cf54b52abbdbf6720cc1a75375e6ba52017e9b79d4e6099907994631acfa30ba6459251258a'
